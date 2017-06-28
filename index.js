77e233181f2a6b8b14892813668d0b0c0992b62e
刘祺 <qil@jumei.com>
2017-06-28 18:13:56 +0800

    fix code style

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var chalk = require('chalk');
 var isBrowser = require('is-in-browser').default;
 var os = require('os');
-var support = isBrowser || process.platform !== 'win32' || parseFloat(os.release()) >= 10
+var support = isBrowser || process.platform !== 'win32' || parseFloat(os.release()) >= 10;
 
 var main = {
 	info: chalk.blue('ℹ'),
