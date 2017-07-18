07b2b58b3b7f4532116278cffaba7aac1ca89cbe
刘祺 <qil@jumei.com>
2017-07-18 16:02:28 +0800

    fix testcase

--- test.js
+++ test.js
@@ -8,7 +8,19 @@ Object.keys(logSymbols).forEach(function (el) {
 });
 console.log('');
 
-test('returns log symbols', function (t) {
-	t.assert(chalk.stripColor(logSymbols.success) === '✔' ||
-	         chalk.stripColor(logSymbols.success) === '√');
-});
+if(process.env.CI){
+	test('returns log symbols', function (t) {
+		t.assert(chalk.stripColor(logSymbols.success) === '\u{2714}\u{fe0f}');
+		process.env.CI = null;
+	});
+} else {
+	test('returns log symbols', function (t) {
+		t.assert(chalk.stripColor(logSymbols.success) === '✔' ||
+				chalk.stripColor(logSymbols.success) === '√');
+	});
+	if (chalk.enabled) {
+		test('log symbols has color', function (t) {
+			t.assert(chalk.hasColor(logSymbols.success));
+		});
+	}
+}
