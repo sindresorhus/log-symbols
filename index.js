9e8577d3327a18c68ade3a400c6963a5a6c9a9f3
刘祺 <qil@jumei.com>
2017-09-06 15:00:10 +0800

    close #11

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
+const cp = require('child_process');
 const chalk = require('chalk');
+
+const CpdePage = {
+	932: true,
+	936: true,
+	949: true,
+	950: true
+};
 
 const isSupported = (() => {
 	if (process.platform !== 'win32' || process.env.CI) {
@@ -12,7 +20,17 @@ const isSupported = (() => {
 		return false;
 	}
 
-	return fs.existsSync(path.join(process.env.windir, 'Fonts/seguiemj.ttf'));
+	if (process.env.VSCODE_CWD) {
+		return true;
+	}
+
+	if (fs.existsSync(path.join(process.env.windir, 'Fonts/seguiemj.ttf'))) {
+		const codepage = cp.spawnSync('chcp').stdout.toString().replace(/.*?(\d+)\s*/, '$1');
+		if (CpdePage[codepage]) {
+			return true;
+		}
+	}
+	return false;
 })();
 
 const main = {
