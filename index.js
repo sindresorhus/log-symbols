f399c862812ef82eff2a37e37d759e08858befc4
Brandon Smith <brandon93s@gmail.com>
2018-01-21 18:43:53 -0500

    Improve Windows support (#16)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const chalk = require('chalk');
 
-const isSupported = process.platform !== 'win32' || process.env.CI || process.env.VSCODE_PID;
+const isSupported = process.platform !== 'win32' || process.env.CI || process.env.TERM === 'xterm-256color';
 
 const main = {
 	info: chalk.blue('ℹ'),
